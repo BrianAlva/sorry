e3791506dd275a4b5af6baebef45145984795605
Alternius <stevenallen291@gmail.com>
2021-11-10 12:46:27 -0500

    Add cards and draw card on click

--- src/main/java/csc331/sorry/SorryController.java
+++ src/main/java/csc331/sorry/SorryController.java
@@ -1,16 +1,27 @@
 package csc331.sorry;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 public class SorryController {
 
     @FXML
+    private ImageView cardImage;
+
+    @FXML
     private GridPane gridPane;
 
     @FXML
-    public void initialize() {
-        gridPane.setStyle("-fx-background-image: url('');");
+    private Label turnLabel;
+
+    @FXML
+    void onCardClicked(MouseEvent event) {
+        int card = (int)(Math.random() * 12 + 1);
+        cardImage.setImage(new Image(String.valueOf(getClass().getResource("cards/" + card + ".png"))));
     }
 
 }
